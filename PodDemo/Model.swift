ec629be415e5486f3f10a8750eaf0eaf2ff54615
dinesh sharma <dineshsharma@dineshs-Mac-mini.local>
2017-05-21 14:05:01 +0530

    add project

---
+++ PodDemo/Model.swift
@@ -0,0 +1,18 @@
+//
+//  Model.swift
+//  PodDemo
+//
+//  Created by dinesh sharma on 21/05/17.
+//  Copyright © 2017 dinesh sharma. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIView {
+    
+    func setBackgroundColor() {
+        self.backgroundColor = UIColor.red
+    }
+}
